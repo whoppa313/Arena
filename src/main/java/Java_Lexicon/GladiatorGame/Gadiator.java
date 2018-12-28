520af5e5fbb1c64ae88214805b4f0fde970e9e5e
Victor_506 <viho98001@utb.vaxjo.se>
2018-12-28 15:05:01 +0100

    first upload

---
+++ src/main/java/Java_Lexicon/GladiatorGame/Gadiator.java
@@ -0,0 +1,29 @@
+package Java_Lexicon.GladiatorGame;
+
+
+
+//  What i need for this Game to work:
+/*
+ * 1. I need a random Name Generator. ( Optional )
+ * 2. I need a random Number Generator.
+ * 3. I need to generate an Enemy gladiator.
+ * 4. I need to make the player able to create, enter and store a name for their Gladiator and any other attributes.
+ * 5. I need to make  the Gladiators to store the name given as long as they are alive.
+ * 6. I need to make their HP go down when either the player or gladiator wins the round depending on who get the bigger random number.
+ * 7. I need to generate a new enemy when the player wins.
+ * 8. I need to make sure the player and enemy got the same health at the start of a new round.
+ * 9. I need to store and add to the score the player gets for each win or when the player dies
+ * 10.I need to make the player able to end the game when the player wants to and show it current score.
+ * 11. ...whatever i can come up with i guess ¯\_(ツ)_/¯
+ */
+
+
+
+
+
+public class Gadiator 
+
+
+{
+
+}
